bf93ab133d4dd942b65c56c8ae71f3b1a1408bb9
Ashish Arora <ashish007722@gmail.com>
2019-12-05 01:07:19 +0530

    fetching data from firebase db for attraction activity
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/kec/hackathon/skimilan/AttractionActivity.kt
+++ app/src/main/java/com/kec/hackathon/skimilan/AttractionActivity.kt
@@ -1,12 +1,38 @@
 package com.kec.hackathon.skimilan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_attraction.*
+
 
 class AttractionActivity : AppCompatActivity() {
+
+    val TAG = "AttractionActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_attraction)
+
+        val databaseReference:DatabaseReference = FirebaseDatabase.getInstance().getReference("attraction")
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) { // This method is called once with the initial value and again
+            // whenever data at this location is updated.
+                val nameAttractionOne = dataSnapshot.child("attraction1").child("name").getValue().toString()
+                val descriptionAttractionOne = dataSnapshot.child("attraction1").child("description").getValue().toString()
+
+                AttractionTextView.text = nameAttractionOne
+                descriptionTextView.text = descriptionAttractionOne
+
+                Log.d(TAG, "Value is: $nameAttractionOne $descriptionAttractionOne")
+            }
+
+            override fun onCancelled(error: DatabaseError) { // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 }
