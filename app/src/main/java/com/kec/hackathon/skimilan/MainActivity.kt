f7f94a930f029685d8dbb0d16b45a6e890c010ce
Ashish0077 <ashish007722@gmail.com>
2019-12-04 19:16:47 +0530

    second commit
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/kec/hackathon/skimilan/MainActivity.kt
+++ app/src/main/java/com/kec/hackathon/skimilan/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kec.hackathon.skimilan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,19 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mapButton.setOnClickListener {View->
-
+            val mapActivityIntent : Intent =  Intent(this, MapActivity::class.java)
+            startActivity(mapActivityIntent)
         }
 
         emergencyButton.setOnClickListener{View->
-
+            val emergencyActivityIntent : Intent = Intent(this, EmergencyActivity::class.java)
+            startActivity(emergencyActivityIntent)
         }
 
         attractionButton.setOnClickListener {View->
-
+            val attractionActivityIntent : Intent = Intent(this, AttractionActivity::class.java)
+            startActivity(attractionActivityIntent)
         }
 
         barcodeButton.setOnClickListener {View->
-
+            val barcodeActivityIntent : Intent = Intent(this,  BarcodeActivity::class.java)
+            startActivity(barcodeActivityIntent)
         }
     }
 }
