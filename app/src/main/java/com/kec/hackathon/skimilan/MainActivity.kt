1db219f72d5255656cd557185143d72da9cfef03
Ashish0077 <ashish007722@gmail.com>
2019-12-04 12:32:32 +0530

    initial commit
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

---
+++ app/src/main/java/com/kec/hackathon/skimilan/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.kec.hackathon.skimilan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mapButton.setOnClickListener {View->
+
+        }
+
+        emergencyButton.setOnClickListener{View->
+
+        }
+
+        attractionButton.setOnClickListener {View->
+
+        }
+
+        barcodeButton.setOnClickListener {View->
+
+        }
+    }
+}
