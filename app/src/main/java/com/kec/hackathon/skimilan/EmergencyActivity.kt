ed7cdf22b654389b5b7f76a6a82ab1ecfd0fcc98
Ashish0077 <ashish007722@gmail.com>
2019-12-05 05:15:45 +0530

    emergency and map activity added Picaso library
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/kec/hackathon/skimilan/EmergencyActivity.kt
+++ app/src/main/java/com/kec/hackathon/skimilan/EmergencyActivity.kt
@@ -2,11 +2,43 @@ package com.kec.hackathon.skimilan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_attraction.*
+import kotlinx.android.synthetic.main.activity_emergency.*
 
 class EmergencyActivity : AppCompatActivity() {
+
+    val TAG = "Emergency Activity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emergency)
+
+        val databaseReference:DatabaseReference = FirebaseDatabase.getInstance().getReference("emergency")
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) { // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val medNum1 = dataSnapshot.child("Medical").child("number1").getValue().toString()
+                val medNum2 = dataSnapshot.child("Medical").child("number2").getValue().toString()
+
+                medNumber1.text = medNum1
+                medNumber2.text = medNum2
+
+                val lostNum1 = dataSnapshot.child("lostAndFound").child("number1").getValue().toString()
+                val lostNum2 = dataSnapshot.child("lostAndFound").child("number2").getValue().toString()
+
+                lostNumber1.text = lostNum1
+                lostNumber2.text = lostNum2
+
+                Log.d(TAG, "Value is: $lostNum1 $lostNum2 $medNum1 $medNum2")
+            }
+
+            override fun onCancelled(error: DatabaseError) { // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+
     }
 }
--- app/src/main/java/com/kec/hackathon/skimilan/MapActivity.kt
+++ app/src/main/java/com/kec/hackathon/skimilan/MapActivity.kt
@@ -1,12 +1,44 @@
 package com.kec.hackathon.skimilan
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_attraction.*
+import kotlinx.android.synthetic.main.activity_map.*
 
 class MapActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val databaseReference = FirebaseDatabase.getInstance().getReference("map")
+
+        val alertDialog = ProgressDialog(this@MapActivity)
+        alertDialog.setTitle("Loading Map")
+        alertDialog.setMessage("Please wait while the map is loading")
+        alertDialog.show()
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) { // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val mapUrl = dataSnapshot.child("map1").getValue().toString();
+                Picasso.get().load(mapUrl).into(mapImageView)
+                alertDialog.dismiss()
+                Log.d("MapActivity", "Value is: $mapUrl")
+            }
+
+            override fun onCancelled(error: DatabaseError) { // Failed to read value
+                Log.w("MapActivity", "Failed to read value.", error.toException())
+            }
+        })
+
     }
 }
